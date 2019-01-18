51a0a8addcd74c556cb8f35fa66ff737091a02c1
Adriana Crescenzi <adrianacrescenzi@yahoo.com>
2019-01-18 17:24:10 -0500

    commit

--- bubblesort.js
+++ bubblesort.js
@@ -1,4 +1,19 @@
-function bubbleSort(array) {
-
-  return array;
+function bubbleSort(array) { 
+  for(let i = 0; i < array.length - 1; i++){
+    let slice = [];
+    if(i === array.length - 2){
+       slice = array.slice(i);
+    } else {
+       slice = array.slice(i ,i + 2)
+    }
+  console.log(swap(slice));
+  }
+  
 }
+
+
+function swap(slice){
+   return slice
+}
+
+bubbleSort([2,4,9,7])
