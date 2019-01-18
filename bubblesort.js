44df10f3383c34f059599237debf70be78b35b2f
Cassie Rosner <cerosner@gmail.com>
2019-01-18 16:39:30 -0500

    began writing test specs

---
+++ bubblesort.js
@@ -0,0 +1,4 @@
+function bubbleSort(array) {
+
+  return array;
+}
---
+++ bubblesort.spec.js
@@ -0,0 +1,12 @@
+const swapCounter = 0;
+const comparisonCounter = 0;
+
+describe('Bubble Sort', function(){
+  it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+});
+
+  it('handles a single input', function(){
+    expect( bubbleSort([0]) ).toEqual(bubbleSort.length = 1);
+  });
